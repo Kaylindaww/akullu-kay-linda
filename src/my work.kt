c11681ace5f0df170dc15ba46e2b4410082b0c19
Iminzah <sharroniminzah@gmail.com>
2021-03-12 10:06:59 +0300

    akullu kay linda

---
+++ src/my work.kt
@@ -0,0 +1,7 @@
+fun main(){var intro=("hello i am kay, 20")
+
+}
+fun intro(name:String,age:Int){
+    println("hello,my name is kay $ 20")
+
+}
---
+++ src/strings.kt
@@ -0,0 +1,26 @@
+fun main(){
+name()
+    kay("linda",20)
+    println(food("ugali"))
+    println(get("kay linda","that is me!"))
+}
+fun name(){
+    var name ="AkiraChix"
+    print(name[0])
+    print(name[2])
+    println(name[3])
+}
+fun kay(name:String,age:Int){
+    var x="linda"
+    var y=20
+    print("Hii my name is $name and i am $age years")
+
+}
+fun food(name:String):Int{
+    var foodlength=name.length
+    return foodlength
+}
+fun get(name:String,Output:String){
+    if (name=="kay linda")
+        println(Output)
+}
